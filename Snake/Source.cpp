62b46bb1bdb480cf1a36c9bb8d5c81406dbddd22
RobotRyanYu <fai5121988@yahoo.com.hk>
2019-01-10 17:11:32 +0800

    [UPDATE] snake_game
    - Add UpdateGameMap() for obtaining the unoccupied grid for resetting the fruit pos to avoid infinite loop

--- Snake/Source.cpp
+++ Snake/Source.cpp
@@ -35,32 +35,44 @@ private:
 
 	std::vector<Pt2i> snake_pos;
 
+	std::vector<std::vector<short>> map;
+
 	Pt2i fruit_pos;
 
 private:
 
+	void Display();
+	
+	void Input();
+	
+	void Logic();
+
 	void ResetFruitPos();
+	
 	void UpdateSnakePos();
+	
 	Pt2i GetNewSnakeHeadPos(const int dir);
 
 	bool CheckBodyCollision();
+
 	bool CheckBoundaryCollision();
+
+	void UpdateGameMap();
+
+	void ResetMap();
 
 public:
 
 	SnakeGame(const int display_width, const int display_height);
 	
-	void Display();
-	void Input();
-	void Logic();
+	void Run();
 };
 
 int main() {
 
 	SnakeGame snake_game(20, 20);
 	while(1){
-		snake_game.Input();
-		snake_game.Display();
+		snake_game.Run();
 	}
 	std::getchar(); std::getchar();
 	return 0;
@@ -77,9 +89,16 @@ SnakeGame::SnakeGame(const int display_width, const int display_height) {
 	snake_pos[0].x = this->display_width / 2;
 	snake_pos[0].y = this->display_height / 2;
 
+	UpdateGameMap();
+
 	ResetFruitPos();
 
 	scores = 0;
+}
+
+void SnakeGame::Run() {
+	Input();
+	Display();
 }
 
 void SnakeGame::Display() {
@@ -161,6 +180,8 @@ void SnakeGame::Input() {
 
 void SnakeGame::Logic() {
 
+	UpdateGameMap();
+
 	if (snake_pos[0].x == fruit_pos.x && snake_pos[0].y == fruit_pos.y){
 		is_ate_fruit = true;
 		scores += 10;
@@ -176,21 +197,32 @@ void SnakeGame::ResetFruitPos() {
 	std::srand(time(NULL));
 	bool is_okay = true;
 
-	do {
-		fruit_pos.x = rand() % (this->display_width - 1);
-		fruit_pos.x = (fruit_pos.x == 0) ? 1 : fruit_pos.x;
-
-		fruit_pos.y = rand() % (this->display_height - 1);
-		fruit_pos.y = (fruit_pos.y == 0) ? 1 : fruit_pos.y;
-
-		for (int i = 0; i < snake_pos.size(); i++) {
-			if (fruit_pos.x == snake_pos[i].x && fruit_pos.y == snake_pos[i].y) {
-				is_okay = false;
-				break;
-			}
+	int num_of_unoccupied_grid = 0;
+	for (int i = 0; i < map.size(); i++) {
+		for (int j = 0; j < map[i].size(); j++) {
+			if (map[i][j] == 1) continue;
+			if (i == 0 || i == display_height - 1) continue;
+			if (j == 0 || j == display_width - 1) continue;
+			num_of_unoccupied_grid++;
 		}
+	}
 
-	} while (!is_okay);
+	std::vector<std::pair<int, int>> unoccupied_grid_idx(num_of_unoccupied_grid);
+
+	for (int i = 0, k = 0; i < map.size(); i++) {
+		for (int j = 0; j < map[i].size(); j++) {
+			if (map[i][j] == 1) continue;
+			if (i == 0 || i == display_height - 1) continue;
+			if (j == 0 || j == display_width - 1) continue;
+			unoccupied_grid_idx[k++] = std::pair<int, int>(i, j);
+		}
+	}
+
+
+	int rand_idx = rand() % num_of_unoccupied_grid;
+	fruit_pos.y = unoccupied_grid_idx[rand_idx].first;
+	fruit_pos.x = unoccupied_grid_idx[rand_idx].second;
+
 }
 
 void SnakeGame::UpdateSnakePos() {
@@ -234,10 +266,10 @@ Pt2i SnakeGame::GetNewSnakeHeadPos(const int dir) {
 	else
 		return new_pos;
 
-	new_pos.x = (new_pos.x < 0) ? -1 : new_pos.x;
-	new_pos.y = (new_pos.y < 0) ? -1 : new_pos.y;
-	new_pos.x = (new_pos.x > display_width) ? display_width : new_pos.x;
-	new_pos.y = (new_pos.y > display_height) ? display_height : new_pos.y;
+	new_pos.x = (new_pos.x < 0) ? 0 : new_pos.x;
+	new_pos.y = (new_pos.y < 0) ? 0 : new_pos.y;
+	new_pos.x = (new_pos.x > display_width - 1) ? display_width - 1: new_pos.x;
+	new_pos.y = (new_pos.y > display_height - 1) ? display_height - 1: new_pos.y;
 
 	return new_pos;
 }
@@ -265,4 +297,29 @@ bool SnakeGame::CheckBodyCollision() {
 	}
 
 	return is_collision;
+}
+
+void SnakeGame::UpdateGameMap() {
+
+	ResetMap();
+
+	for (int i = 0; i < snake_pos.size(); i++) 
+		map[snake_pos[i].y][snake_pos[i].x] = 1;
+
+}
+
+void SnakeGame::ResetMap() {
+	
+	if (map.empty()) {
+		map.resize(this->display_height);
+		for (int i = 0; i < map.size(); i++)
+			map[i].resize(this->display_width, 0);
+	}
+	else {
+		for (int i = 0; i < map.size(); i++) {
+			for (int j = 0; j < map[i].size(); j++) {
+				map[i][j] = 0;
+			}
+		}
+	}
 }
